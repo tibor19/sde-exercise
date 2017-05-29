49ec5a0c90eccd3418e39c89e6779f73085420d1
Tiberiu Covaci <tibi@covaci.se>
2017-05-29 13:13:53 +0100

    Add RulesViewModel

---
+++ ViewModel/RulesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCoreOAuth2Sample.Model;
+using System.Collections.Generic;
+
+namespace AspNetCoreOAuth2Sample.ViewModel
+{
+    public class RulesViewModel
+    {
+        public IEnumerable<Rule> Rules { get; set; }
+        public IEnumerable<string> Clients { get; set; }
+    }
+}
